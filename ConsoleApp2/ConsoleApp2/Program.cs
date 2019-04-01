916538326f8425879b84e32762f201247e7d5792
BakonyiGergőIstván <BakonyiGergIstvn@10.1.16.239>
2019-04-01 23:54:55 +0200

    Webprogramozás és haladó fejlesztés java féléves feladat tutorial kódja.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private const string V = @"http://localhost:8080/wphf/CarQuery?brand=opel";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var client = new WebClient())
+            {
+                string xml_text = client.DownloadString(V);
+                XDocument doc = XDocument.Parse(xml_text);
+                Console.WriteLine(doc);
+                Console.ReadLine();
+            
+            }
+
+        }
+    }
+}
